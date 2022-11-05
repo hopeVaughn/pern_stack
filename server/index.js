dbe2a27f292c25ca416c9157ae52599acca19aa8
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-05 10:35:45 -0700

    add post route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,41 @@ const pool = require("./db")
 app.use(cors());
 app.use(express.json())// => allows access to req.body
 
+
+//routes
+// -----------------------
+//get all todos
+
+
+// -------------------------
+//get a single todo
+
+
+//----------------------------
+//create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", [description]);
+
+    res.json(newTodo.rows[0])
+    // res.json(req.body)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+//--------------------------
+//update a todo
+
+
+//-----------------------------------
+//delete a todo
+
+
+//------------------------------------------
+
+
 app.listen(5000, () => {
   console.log("Server listening on port 5000");
 });
