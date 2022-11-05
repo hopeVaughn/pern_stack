75a67797072ad0b2b73fbf093acd02a277b47937
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-05 10:55:45 -0700

    add put and delete routes

--- server/index.js
+++ server/index.js
@@ -31,7 +31,6 @@ app.get("/todos/:id", async (req, res) => {
     console.error(err.message)
   }
 })
-
 //----------------------------
 //create a todo
 app.post("/todos", async (req, res) => {
@@ -45,15 +44,31 @@ app.post("/todos", async (req, res) => {
     console.error(err.message)
   }
 })
-
 //--------------------------
 //update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query("UPDATE todo SET DESCRIPTION =$1 WHERE todo_id =$2", [description, id]);
 
-
+    res.json("Todo was updated")
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 //-----------------------------------
 //delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id])
 
-
+    res.json("Todo was deleted")
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 //------------------------------------------
 
 
