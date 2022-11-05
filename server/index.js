1db95778b2dff5a236316e863457dda99f9980de
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-05 10:44:17 -0700

    add single route

--- server/index.js
+++ server/index.js
@@ -10,11 +10,27 @@ app.use(express.json())// => allows access to req.body
 //routes
 // -----------------------
 //get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
 
-
+    res.json(allTodos.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 // -------------------------
 //get a single todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
 
+    res.json(todo.rows[0])
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 //----------------------------
 //create a todo
