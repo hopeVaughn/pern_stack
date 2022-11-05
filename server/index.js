2b990cdf7785a0cf944a1b31e3895538107d19bb
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-05 10:25:09 -0700

    add connection to psql database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
-
+const pool = require("./db")
 //middleware
 app.use(cors());
+app.use(express.json())// => allows access to req.body
 
 app.listen(5000, () => {
-  console.log("Server listeing on port 5000");
-})
+  console.log("Server listening on port 5000");
+});
